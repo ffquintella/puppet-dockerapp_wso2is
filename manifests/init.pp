1d534713cb77664906b3ba66f54d37712a2bc6a0
Felipe F Quintella <felipe@quintella.com>
2019-08-27 18:06:46 -0300

    INC

--- manifests/init.pp
+++ manifests/init.pp
@@ -495,6 +495,7 @@ class dockerapp_wso2is (
           'enable_ha'    => $enable_ha,
           'cluster_name' => $ha_cluster_name,
           'ha_members'   => $ha_members,
+          'ip_address'   => $::ip,
           }),
         notify  => Docker::Run[$service_name],
         require => File[$conf_configdir],
